a5c48d7611595a487e8f100d1336929443ad6fae
saurabhkjha <saurabh.simpy@gmail.com>
2022-09-28 06:04:17 +0530

    search popup

--- components/searchPopup.tsx
+++ components/searchPopup.tsx
@@ -7,10 +7,21 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import SearchIcon from '@mui/icons-material/Search';
 import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 import Typography from '@mui/material/Typography';
-import { Grid, Paper, Stack } from '@mui/material';
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  InputBase,
+  OutlinedInput,
+  Paper,
+  Stack,
+} from '@mui/material';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -42,6 +53,18 @@ export interface SearchPopupProps {
   title: string;
   onClose: () => void;
 }
+
+const tenants = [
+  'Homeoffice',
+  'GEC',
+  'Global',
+  'International',
+  'Logistic',
+  "Sam's",
+  "Sam's & Walmart",
+  'Walmart',
+  'Walmart Health',
+];
 
 const BootstrapDialogTitle = (props: DialogTitleProps) => {
   const { children, onClose, onFullScreen, fullScreen, ...other } = props;
@@ -81,6 +104,27 @@ const BootstrapDialogTitle = (props: DialogTitleProps) => {
   );
 };
 
+const AvailableList = ({ name }) => {
+  return (
+    <Paper
+      component="form"
+      sx={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        borderRadius: '0px',
+        pb: 1,
+      }}
+    >
+      <Item>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox />} label={name} />
+        </FormGroup>
+      </Item>
+    </Paper>
+  );
+};
+
 export default function SearchPopup(props: SearchPopupProps) {
   const { mode, title, onClose } = props;
   const [open, setOpen] = React.useState(false);
@@ -106,7 +150,8 @@ export default function SearchPopup(props: SearchPopupProps) {
         aria-labelledby="customized-dialog-title"
         open={open}
         fullScreen={fullScreen}
-        maxWidth="md"
+        fullWidth={true}
+        maxWidth={'lg'}
       >
         <BootstrapDialogTitle
           id="customized-dialog-title"
@@ -140,27 +185,67 @@ export default function SearchPopup(props: SearchPopupProps) {
                 >
                   AVAILABLE
                 </Typography>
-                <Item>Item 1</Item>
-                <Item>Item 2</Item>
-                <Item>Item 3</Item>
+                <Paper
+                  component="form"
+                  sx={{
+                    display: 'flex',
+                    width: '100%',
+                    alignItems: 'center',
+                    borderRadius: '0px',
+                    pb: 1,
+                    borderBottom: '1px solid #D3D3D3',
+                  }}
+                >
+                  <Checkbox />
+                  <OutlinedInput
+                    startAdornment={
+                      <IconButton>
+                        <SearchIcon />
+                      </IconButton>
+                    }
+                    sx={{
+                      display: 'flex',
+                      width: '100%',
+                      p: 0,
+                      mr: 2,
+                      ml: 2,
+                    }}
+                  />
+                </Paper>
+                {tenants.map((tenant) => (
+                  <AvailableList name={tenant} />
+                ))}
               </Stack>
             </Grid>
 
             <Grid
-              item
               md={6}
               style={{
-                paddingLeft: '10px',
                 borderStyle: 'solid',
                 borderColor: 'rgba(0, 0, 0, 0.12)',
                 borderWidth: '0 0 0 1px',
               }}
             >
-              <Typography gutterBottom>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur
-                et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus
-                dolor auctor.
-              </Typography>
+              <Stack spacing={2}>
+                <Typography
+                  gutterBottom
+                  sx={{
+                    flex: '1 1 100%',
+                    background: '#D3D3D3',
+                    color: '#A9A9A9',
+                    fontWeight: 'bold',
+                    fontSize: '1em',
+                    paddingX: '1em',
+                    paddingY: '.5em',
+                  }}
+                >
+                  SELECTED
+                </Typography>
+
+                <Item>Item 1Item 1Item 1Item 1Item 1Item 1Item 1</Item>
+                <Item>Item 2</Item>
+                <Item>Item 3</Item>
+              </Stack>
             </Grid>
           </Grid>
         </DialogContent>
